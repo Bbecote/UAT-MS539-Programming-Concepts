08ce784b7ccf96f874c340d030dc788bfad2fab1
Bbecote <Briant.becote@gmail.com>
2024-05-18 20:10:28 -0700

    Custom Buttons

--- CrapsSimC#/obj/Debug/net8.0-windows/CrapsSimC#.AssemblyInfo.cs
+++ CrapsSimC#/obj/Debug/net8.0-windows/CrapsSimC#.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrapsSimC#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adacccd0d7f5b6a90e0259b38223562417019838")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01f5a0d69476fecbe83923d94c86aa5b77e21f71")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrapsSimC#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrapsSimC#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
