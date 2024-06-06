5cbed1a627d0109e2cd5b70d86aad288a3aca108
bbecote <briant.becote@gmail.com>
2024-06-06 14:20:59 -0700

    Initial

--- Budget/obj/Debug/net8.0-windows/Budget.AssemblyInfo.cs
+++ Budget/obj/Debug/net8.0-windows/Budget.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("CrapsSimC#")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Budget")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b59c24d88eae92c7d2452a3e332da673cc361e1f")]
-[assembly: System.Reflection.AssemblyProductAttribute("CrapsSimC#")]
-[assembly: System.Reflection.AssemblyTitleAttribute("CrapsSimC#")]
+[assembly: System.Reflection.AssemblyProductAttribute("Budget")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Budget")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
